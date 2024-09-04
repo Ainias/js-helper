8e2f3fb172edca62d8adab644b030deae9c90e82
sguenter <sguenther@churchtools.de>
2024-09-04 17:48:41 +0200

    updated types

--- dist/TypeHelper.d.ts
+++ dist/TypeHelper.d.ts
@@ -14,4 +14,5 @@ export type Override<T1, T2> = Omit<T1, keyof T2> & T2;
 export type Conditional<T1, T2> = Partial<T1> | (T1 & T2);
 export type ValueOf<T> = T[keyof T];
 export type Recursive<T> = T | T[] | Recursive<T>[];
+export type Awaitable<T> = T | Promise<T>;
 export type URecord<Key extends number | string | symbol, Value> = Record<Key, Value | undefined>;
